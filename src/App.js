640432301a125000860b90ff7f5d373da4842ed3
vaskania <vaskania26@gmail.com>
2021-08-27 23:12:33 +0400

    deploy react app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import CartProvider from "./store/CartProvider";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandle = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandle} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
